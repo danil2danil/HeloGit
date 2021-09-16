540a51388495ec523fea00eca56f9289e8df69b1
Борунов Данил <danilborunov26@mail.ru>
2021-09-16 16:30:23 +0300

    переделан z2(реализадия двумерного динамического массива с рандомизацией его элементов)

--- z2/Z2.cpp
+++ z2/Z2.cpp
@@ -1,18 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <conio.h>
+#include <malloc.h>
+#include <time.h>
 
-#define SIZEOFMASS 10
 
 int main() {
-	srand(time(NULL));
-	int i, a[SIZEOFMASS];
-	printf("initial array: ");
-	for (i = 0; i < SIZEOFMASS; i++)
+
+	int max_size, max_size2, i, j;
+	srand((time(NULL)));
+	printf("max number of str? ");
+	scanf_s("%d", &max_size);
+	printf("\nmax number of coll? ");
+	scanf_s("%d", &max_size2);
+
+	int** a = new int* [max_size];
+
+	for (i = 0; i < max_size; i++)
 	{
-		a[i] = rand() % 100;
-		printf("%d ", a[i]);
+		a[i] = new int[max_size2];
+	}
+
+
+	for (i = 0; i < max_size; i++)
+	{
+		for (j = 0; j < max_size2; j++)
+		{
+			a[i][j] = rand() % 10;
+		}
+	}
+
+
+
+	printf("\nyour array: \n");
+	for (i = 0; i < max_size; i++)
+	{
+		for (j = 0; j < max_size2; j++)
+		{
+			printf("%3d", a[i][j]);
+		}
+		printf("\n");
 	}
 	_getch();
 }
