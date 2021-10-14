6173d85825f17bca196794384c773853dcf535e8
Борунов Данил <danilborunov26@mail.ru>
2021-10-14 20:48:26 +0300

    Добавьте файлы проекта.

---
+++ Z1/Z1.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <cstdlib>
+#include <windows.h>
+#include <conio.h>
+#include <ctime>
+#include <cmath>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+int max_size = 7, i, j, start;
+bool* vektor = (bool*)malloc(max_size * sizeof(bool));
+
+void DFS(int st, int** b, int num_of_elem, bool* visited) {
+	int g;
+	printf("%d ", st+1);
+	visited[st] = true;
+	for (g = 0; g < max_size; g++) {
+		if (b[st][g] != 0 && !visited[g])
+			DFS(g, b, num_of_elem, visited);
+	}
+}
+
+void DFS_noRekr(int st, int** b, int num_of_elem)
+{
+	queue<int> Q;
+
+	int* mass;
+	int t;
+
+	mass = (int*)malloc(num_of_elem * sizeof(int));
+	for (int i = 0; i < num_of_elem; i++) {
+		mass[i] = 0;
+	}
+	Q.push(st);
+	mass[st] += 1;
+	while (!Q.empty())
+	{
+		t = Q.front();
+		Q.pop();
+		printf("%d ", t + 1);
+		for (int i = 0; i < num_of_elem; i++)
+			if (b[t][i] && !mass[i])
+			{
+				Q.push(i);
+				mass[i] += 1;
+			}
+	}
+}
+
+int main() {
+	
+	setlocale(LC_ALL, "Rus");
+	srand(time(NULL));
+	
+
+	int** a = new int* [max_size];
+	for (i = 0; i < max_size; i++)
+	{
+		a[i] = new int[max_size];
+	}
+
+	for (i = 0; i < max_size; i++)
+	{
+		for (j = i + 1; j < max_size; j++)
+		{
+			a[i][j] = rand() % 2;
+			a[j][i] = a[i][j];
+		}
+	}
+
+	for (i = 0; i < max_size; i++)
+	{
+		vektor[i] = false;
+		for (j = 0; j < max_size; j++)
+		{
+			if (i == j)
+				a[i][j] = 0;
+			printf("%2d", a[i][j]);
+		}
+		printf("\n");
+	}
+	
+	
+	///////////////////////////////////////
+
+	printf("\nОбход графа в глубину методом рекурсии\ncтартовая вершина - ");
+	scanf_s("%d", &start);
+	printf("Вывод: ");
+	DFS(start-1, a, max_size, vektor);
+
+	printf("\n");
+
+	printf("\nОбход графа в глубину методом рекурсии\ncтартовая вершина - ");
+	scanf_s("%d", &start);
+	printf("Вывод: ");
+	DFS_noRekr(start-1, a, max_size);
+
+	_getch();
+}
